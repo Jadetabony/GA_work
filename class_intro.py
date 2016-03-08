465df14d80a0b350717624b5d1e8c7bda156da5a
Jadetabony <jat257@cornell.edu>
2016-03-08 12:01:38 -0800

    Update class_intro.py

--- class_intro.py
+++ class_intro.py
@@ -1,5 +1,5 @@
 class Introduction(object):
-
+    class_names = {}
 
     def __init__(self, name, job, origin, hobby1, hobby2, fun_fact):
         self.name=name
@@ -8,12 +8,12 @@ class Introduction(object):
         self.hobby1 = hobby1
         self.hobby2 = hobby2
         self.fun_fact= fun_fact
-        class_names = []
+
 
     def add_name(self):
-        if not self.name in self.class_names:
-            self.class_names.append(self.name)
+        if not self.name in Introduction.class_names:
             print self.name, "has not been introduced!"
+            Introduction.class_names[self.name] = "not introduced"
         else:
             print self.name, "has been introduced."
 
@@ -22,18 +22,14 @@ class Introduction(object):
         if self.job[0] in ('a', 'e', 'i', 'o', 'u'):
             print "Hi! I'm %s!  I'm an %s and I'm orignially from %s." %(self.name, self.job, self.origin)
             print "When not programming, I like %s and %s." %(self.hobby1, self.hobby2)
-            print fun_fact
+            print self.fun_fact
+            Introduction.class_names[self.name] = "introduced"
         else:
             print "Hi! I'm %s!  I'm a %s and I'm orignially from %s." %(self.name, self.job, self.origin),
             print "When not programming, I like %s and %s." %(self.hobby1, self.hobby2)
-            print fun_fact
+            print self.fun_fact
+            Introduction.class_names[self.name] = "introduced"
 
-test = Introduction("","","","","","")
-
-
-jade = Introduction("Jade", "Environmental health consultant", "Louisiana", "rock climbing", "mountaineering", "I grew up in El Salvador.")
-
-jade.add_name()
 
 jade.print_intro()
 
